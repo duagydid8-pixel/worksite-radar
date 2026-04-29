a915a905808a96536fa5148ab073a037c50bf2b7
염효양 <duagydid8@gmail.com>
2026-04-29 13:25:41 +0900

    style: move notifications to top center

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -10,6 +10,8 @@ const Toaster = ({ ...props }: ToasterProps) => {
     <Sonner
       theme={theme as ToasterProps["theme"]}
       className="toaster group"
+      position="top-center"
+      offset={72}
       toastOptions={{
         classNames: {
           toast:
